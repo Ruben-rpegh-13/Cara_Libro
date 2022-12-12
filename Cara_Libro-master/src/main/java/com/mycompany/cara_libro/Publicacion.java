7e205fa60878953fe5170c5f4407eea932cc7194
Ruben-rpegh-13 <rubenpazo@outlook.es>
2022-12-12 12:55:46 +0100

    creacion clase mensaxe

---
+++ Cara_Libro-master/src/main/java/com/mycompany/cara_libro/Publicacion.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.cara_libro;
+
+/**
+ *
+ * @author a21rubenpe
+ */
+public class Publicacion {
+    
+   /* Commit - subir archivo
+      push   - llevar a cabo commit
+      pull refrescar proyecto
+    
+      Proyecto-commit-remote-push SUBIR ARCHIVO
+      Proyecto-commit-remote-pull BAJAR ARCHIVO
+    */
+            
+    
+}
