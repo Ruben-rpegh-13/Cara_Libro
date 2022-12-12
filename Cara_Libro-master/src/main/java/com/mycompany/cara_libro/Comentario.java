8fc8e495fa920aba795d66153535f7b97a73add2
ruben <rubenpazo@outlook.esgit config --global user.name ruben-rpegh-13>
2022-12-12 18:17:18 +0100

    Clase comentario

---
+++ Cara_Libro-master/src/main/java/com/mycompany/cara_libro/Comentario.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.cara_libro;
+import java.util.Date;
+import java.util.Scanner;
+import java.util.Date;
+
+/**
+ *
+ * @author ruben
+ */
+public class Comentario {
+    
+    public Date data;
+    public String texto;
+    Scanner teclado = new Scanner(System.in);
+    
+    public Comentario(String texto) {
+        
+      texto =teclado.nextLine();
+        System.out.println(LocalDate);
+        
+        
+    }
+    
+    
+    
+}
